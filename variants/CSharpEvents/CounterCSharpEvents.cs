26d1187bad71d01650590574ef616ff1fb3e819e
gru97 <fatemeh.shahidani@gmail.com>
2020-08-13 12:12:35 +0430

    implementations

---
+++ variants/CSharpEvents/CounterCSharpEvents.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EventPlay.variants.ObserverPattern;
+
+namespace EventPlay.variants.CSharpEvents
+{
+   
+    public class CounterCSharpEvents: ICounter
+    {
+        public event EventHandler<int> CounterChanged;
+        private int number = 0;
+        public void CountOne()
+        {
+            number++;
+            CounterChanged?.Invoke(this, number);
+        }
+
+        public void StopCounter()
+        {
+            number = 0;
+        }
+    }
+
+    public class Printer
+    {
+        public void Print()
+        {
+            CounterCSharpEvents counterObserverPattern = new CounterCSharpEvents();
+            counterObserverPattern.CounterChanged += OnCounterChanged;
+            //counterObserverPattern.CounterChanged += (object sender, int e) =>Console.WriteLine(e);
+
+            for (int i = 0; i < 10; i++)
+            {
+                counterObserverPattern.CountOne();
+            }
+
+        }
+        private void OnCounterChanged(object sender, int e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    public class DriverForCounterCSharpEvents : IDriver
+    {
+        public void StartCounting()
+        {
+            Printer p=new Printer();
+            p.Print();
+        }
+    }
+}
